e73c7af9c1cb8f9c713b352160653c1070aed1b4
frehburg <43171336+frehburg@users.noreply.github.com>
2023-10-13 16:07:29 +0200

    added new main method to test person struct

--- src/rusty_network/main.rs
+++ src/rusty_network/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    // Use the 'Person' struct from the 'person' module
+    let mut person = person::Person::new("Alice", 25);
+
+    println!("Name: {}", person.get_name());
+    println!("Age: {}", person.get_age());
+
+    person.say_hello();
+    person.have_birthday();
+    println!("New age: {}", person.get_age());
 }
